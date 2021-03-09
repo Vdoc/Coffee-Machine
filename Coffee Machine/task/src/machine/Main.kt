943a93676da0c820e9892d25459b8cf167e0c996
NikoBolt <docnikbolt@gmail.com>
2021-03-09 23:25:55 +0300

    4/6: Buy, fill, take!

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -3,109 +3,124 @@ package machine
 import java.util.*
 
 /*
-Stage 3/6: Estimate the number of servings
+4/6: Buy, fill, take!
  */
 val scanner = Scanner(System.`in`)
 
 fun main() {
-    val data = readInput()
-    val coffeMachine = CoffeMachine(data)
+    val coffeMachine = CoffeMachine()
+    coffeMachine.state.show()
 
-    println("Write how many cups of coffee you will need: ")
-    val n = scanner.nextInt()
-    coffeMachine.makeCoffee(n)
+    print("Write action (buy, fill, take): ")
+    val answer = scanner.next()
+    when (answer) {
+        "take" -> coffeMachine.take()
+        "fill" -> coffeMachine.fill()
+        "buy" -> coffeMachine.buy()
+    }
 }
 
 class CoffeMachine {
-    var water: Int
-    var milk: Int
-    var beans: Int
-    var waterCups: Int
-    var milkCups: Int
-    var beansCups: Int
+    var money: Int = 550
+    var water: Int = 400
+    var milk: Int = 540
+    var beans: Int = 120
+    var disposableCups = 9
 
+    val state: State = State(this)
 
-    constructor(data: Array<Int>) {
-        water = data[0]
-        milk = data[1]
-        beans = data[2]
-        waterCups = water / 200
-        milkCups = milk / 50
-        beansCups = beans / 15
+    fun take() {
+        println("I gave you ${money}")
+        money -= money
+        println()
+        state.show()
     }
 
-    fun makeCoffee(n: Int) {
-        val min = min()
-        if (min >= n) {
-            print("Yes, I can make that amount of coffee")
-            if (min > n) {
-                print("(and even ${min - n} more than that)")
+    fun fill() {
+        print("Write how many ml of water do you want to add: ")
+        val addWwater = scanner.nextInt()
+        print("Write how many ml of milk do you want to add: ")
+        val addMmilk = scanner.nextInt()
+        print("Write how many grams of coffee beans do you want to add: ")
+        val addBeans = scanner.nextInt()
+        print("Write how many disposable cups of coffee do you want to add: ")
+        val addDisposableCups = scanner.nextInt()
+
+        water += addWwater
+        milk += addMmilk
+        beans += addBeans
+        disposableCups += addDisposableCups
+        println()
+        state.show()
+    }
+
+    fun buy() {
+        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+        val n = scanner.nextInt()
+        makeCoffee(n)
+        println()
+        state.show()
+    }
+
+    fun makeCoffee(whatCoffee: Int) {
+        if (state.isReady(whatCoffee)) {
+            brew()
+        }
+    }
+
+    fun brew() {
+        water -= state.preparedWater
+        milk -= state.preparedMilk
+        beans -= state.preparedBeans
+        money += state.preparedMoney
+        disposableCups--
+    }
+}
+
+class State(val coffeMachine: CoffeMachine) {
+    var preparedWater: Int = 0
+    var preparedMilk: Int = 0
+    var preparedBeans: Int = 0
+    var preparedMoney: Int = 0
+
+    fun show() {
+        println("The coffee machine has:\n" +
+                "${coffeMachine.water} of water\n" +
+                "${coffeMachine.milk}  of milk\n" +
+                "${coffeMachine.beans}  of coffee beans\n" +
+                "${coffeMachine.disposableCups}  of disposable cups\n" +
+                "${coffeMachine.money} of money\n")
+    }
+
+    fun isReady(whatCoffee: Int): Boolean {
+        when (whatCoffee) {
+            1 -> {
+                preparedWater = 250
+                preparedMilk = 0
+                preparedBeans = 16
+                preparedMoney = 4
+
             }
-            print("\n")
-        } else {
-            print("No, I can make only $min cups of coffee")
+            2 -> {
+                preparedWater = 350
+                preparedMilk = 75
+                preparedBeans = 20
+                preparedMoney = 7
+            }
+            3 -> {
+                preparedWater = 200
+                preparedMilk = 100
+                preparedBeans = 12
+                preparedMoney = 6
+            }
+            else -> return false
         }
-        make(n)
-    }
-
-    private fun make(n: Int) {
-        repeat(n){
-            water -= 200
-            milk -= 50
-            beans -= 15
-            waterCups--
-            milkCups--
-            beansCups--
+        if (    coffeMachine.water > preparedWater &&
+                coffeMachine.milk > preparedMilk &&
+                coffeMachine.beans > preparedBeans &&
+                coffeMachine.disposableCups > 0) {
+            return true
         }
-    }
-
-    fun min(): Int {
-        var min = waterCups
-        if (milkCups < min) min = milkCups
-        if (beansCups < min) min = beansCups
-        return min
+        else return false
     }
 }
-
-fun readInput(): Array<Int> {
-    println("Write how many ml of water the coffee machine has: ")
-    val water = scanner.nextInt()
-    println("Write how many ml of milk the coffee machine has: ")
-    val milk = scanner.nextInt()
-    println("Write how many grams of coffee beans the coffee machine has: ")
-    val beans = scanner.nextInt()
-    return arrayOf(water, milk, beans)
-}
-
-
-
-/*
-Stage 2/6: Ingredient calculator
-
-fun main() {
-    println("Write how many cups of coffee you will need: ")
-    val scanner = Scanner(System.`in`)
-    val n = scanner.nextInt()
-    val water = 200 * n
-    val milk = 50 * n
-    val beans = 15 * n
-    println("For $n cups of coffee you will need:\n" +
-            "$water ml of water\n" +
-            "$milk ml of milk\n" +
-            "$beans g of coffee beans")
-}
-*/
-
-
-/*
-Stage 1/6: Making coffee
-fun main() {
-    println("Starting to make a coffee\n" +
-            "Grinding coffee beans\n" +
-            "Boiling water\n" +
-            "Mixing boiled water with crushed coffee beans\n" +
-            "Pouring coffee into the cup\n" +
-            "Pouring some milk into the cup\n" +
-            "Coffee is ready!")
-}
-*/
