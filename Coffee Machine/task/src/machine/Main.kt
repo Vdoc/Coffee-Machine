2ca76d82c12dd1ba2e63a887fbfc41e36670e463
NikoBolt <docnikbolt@gmail.com>
2021-03-09 19:39:39 +0300

    Stage 2/6: Ingredient calculator

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,7 +1,28 @@
 package machine
+
+import java.util.*
+
+/*
+Stage 2/6: Ingredient calculator
+ */
+fun main() {
+    println("Write how many cups of coffee you will need: ")
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
+    val water = 200 * n
+    val milk = 50 * n
+    val beans = 15 * n
+    println("For $n cups of coffee you will need:\n" +
+            "$water ml of water\n" +
+            "$milk ml of milk\n" +
+            "$beans g of coffee beans")
+}
+
+
+
 /*
 Stage 1/6: Making coffee
- */
+
 fun main() {
     println("Starting to make a coffee\n" +
             "Grinding coffee beans\n" +
@@ -11,3 +32,4 @@ fun main() {
             "Pouring some milk into the cup\n" +
             "Coffee is ready!")
 }
+*/
