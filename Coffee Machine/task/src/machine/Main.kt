7f3e888415da49ca245bbbd930ef71e8e7be58b3
NikoBolt <docnikbolt@gmail.com>
2021-03-09 19:09:15 +0300

    Stage 1/6: Making coffee

---
+++ Coffee Machine/task/src/machine/Main.kt
@@ -0,0 +1,13 @@
+package machine
+/*
+Stage 1/6: Making coffee
+ */
+fun main() {
+    println("Starting to make a coffee\n" +
+            "Grinding coffee beans\n" +
+            "Boiling water\n" +
+            "Mixing boiled water with crushed coffee beans\n" +
+            "Pouring coffee into the cup\n" +
+            "Pouring some milk into the cup\n" +
+            "Coffee is ready!")
+}
